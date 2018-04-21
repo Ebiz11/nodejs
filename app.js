89a871fad07172cc8bc1792958bb1628bb0b4d19
ebiz <lustriaebiz@gmail.com>
2018-04-21 22:59:25 +0700

    function expressions

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-
-// normal function
-function sayHi(){
-    console.log('hi');
-}
-
-sayHi();
+function callFunction(fun){
+    fun()
+};
 
 // function expression
 var sayBye = function(){
     console.log('bye');   
 }
 
-sayBye();
+callFunction(sayBye);
