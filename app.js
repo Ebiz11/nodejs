fd305470998adfb42d88175ac3ed15c5699b7514
ebiz <lustriaebiz@gmail.com>
2018-04-22 20:43:38 +0700

    readable stream

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var http = require('http');
 
-var server = http.createServer(function(req, res){
-    console.log('request was made: '+ req.url);
+var fs = require('fs');
+
+var myReadStream = fs.createReadStream(__dirname+'/readMe.txt', 'utf8');
+
+myReadStream.on('data', function(chunk){
+    console.log('new chunk received');
+    console.log(chunk);
+})
+
+// var server = http.createServer(function(req, res){
+//     console.log('request was made: '+ req.url);
     
-    res.writeHead(200, {'Content-Type' : 'text/plain'});
-    res.end('hey ebiz');
-});
+//     res.writeHead(200, {'Content-Type' : 'text/plain'});
+//     res.end('hey ebiz');
+// });
 
-server.listen(2028, '127.0.0.1');
+// server.listen(2028, '127.0.0.1');
 
-console.log('now listen to port 2018');
+// console.log('now listen to port 2018');
