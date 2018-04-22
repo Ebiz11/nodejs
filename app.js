30b0857386d9fd581b6e91f7d9d7e333a6962f51
ebiz <lustriaebiz@gmail.com>
2018-04-22 20:15:02 +0700

    read file

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
-var events = require('events');
+var fs = require('fs');
 
-var util = require('util');
+var readMe = fs.readFileSync('readMe.txt', 'utf8');
 
-var Person = function(name){
-    this.name = name;
-};
-
-util.inherits(Person, events.EventEmitter);
-
-var dele = new Person('dele');
-var ali = new Person('ali');
-var eriksen = new Person('eriksen');
-
-var people = [dele, ali, eriksen];
-
-people.forEach(function(person){
-    person.on('speak', function(msg){
-        console.log(person.name + ' said: '+ msg);
-    })
-});
-
-dele.emit('speak', 'hey dudes');
-ali.emit('speak', 'hey dudes');
+console.log(readMe);
