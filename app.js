bb92d5d1377f11c491208956d83844f87f1514a3
ebiz <lustriaebiz@gmail.com>
2018-04-22 20:46:33 +0700

    writeable stream

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var http = require('http');
 var fs = require('fs');
 
 var myReadStream = fs.createReadStream(__dirname+'/readMe.txt', 'utf8');
+var myWriteStream = fs.createWriteStream(__dirname+'/writeMe.txt');
 
 myReadStream.on('data', function(chunk){
     console.log('new chunk received');
-    console.log(chunk);
+    myWriteStream.write(chunk);
 })
 
 // var server = http.createServer(function(req, res){
