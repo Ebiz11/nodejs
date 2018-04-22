a1b5c551f4dae37a6c6af3fd0c202e1e3ea30de5
ebiz <lustriaebiz@gmail.com>
2018-04-22 21:12:35 +0700

    serving json data

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ var fs = require('fs');
 var server = http.createServer(function(req, res){
     console.log('request was made: '+ req.url);
     
-    res.writeHead(200, {'Content-Type' : 'text/html'});
-    var myReadStream = fs.createReadStream(__dirname+'/index.html','utf8');
+    res.writeHead(200, {'Content-Type' : 'application/json'});
+    var myObj = {
+        name: 'Ebiz',
+        job: 'Ninja',
+        age: 24
+    };
 
-    myReadStream.pipe(res);
+    res.end(JSON.stringify(myObj));
 });
 
 server.listen(2028, '127.0.0.1');
