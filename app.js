84197278f16a916212abbd7e7764005a12dce245
ebiz <lustriaebiz@gmail.com>
2018-04-22 20:19:58 +0700

    write and create file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 
-var readMe = fs.readFileSync('readMe.txt', 'utf8');
-
-fs.writeFileSync('writeMe.txt', readMe);
+fs.readFileSync('readMe.txt', 'utf8', function(err, data){
+    console.log(data);
+    fs.writeFile('writeMe.txt', data)
+});
