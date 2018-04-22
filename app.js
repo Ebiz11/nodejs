1ae0de23af2ab236d8a93fbb9bc46e058267759f
ebiz <lustriaebiz@gmail.com>
2018-04-22 21:34:43 +0700

    routing params

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res){
 
 app.get('/contact', function(req, res){
     res.send('this is contact page');
+});
+
+app.get('/profile/:name', function(req, res){
+    res.send('profile name '+ req.params.name);
 })
 
 app.listen(4002);
