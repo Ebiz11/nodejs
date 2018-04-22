13e2fc016ae19ce4f5b38f04c30750e4ab7d3e84
ebiz <lustriaebiz@gmail.com>
2018-04-22 20:28:08 +0700

    delete folder
    sebelum delete folder harus delete file di dalam folder terlebih dahulu

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var fs = require('fs');
 
-fs.mkdir('stuff', function(){ // buat folder dengan nama stuff
-    fs.readFile('readMe.txt', 'utf8', function(err, data){ // baca file readMe.txt
-        fs.writeFile('./stuff/writeMe.txt', data); // buat filewriteMe.txt di dalam folder stuff
-    })
+fs.unlink('./stuff/writeMe.txt', function(){
+    fs.rmdir('stuff');
 })
