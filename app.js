d775c39537ca1a541c6ee0a6a263e243969bb49e
ebiz <lustriaebiz@gmail.com>
2018-04-22 20:25:05 +0700

    create folder

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 var fs = require('fs');
 
-fs.unlink('writeMe.txt');
+fs.mkdir('stuff', function(){ // buat folder dengan nama stuff
+    fs.readFile('readMe.txt', 'utf8', function(err, data){ // baca file readMe.txt
+        fs.writeFile('./stuff/writeMe.txt', data); // buat filewriteMe.txt di dalam folder stuff
+    })
+})
